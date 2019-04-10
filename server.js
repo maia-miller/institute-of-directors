a27f1b5bc647b32182ceb8e81d3a625669ed38b8
Maia <maiamiller25@gmail.com>
2019-04-10 19:35:54 +1200

    Undo wwwhisper

--- server.js
+++ server.js
@@ -2,16 +2,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-//wwwhisper
-const wwwhisper = require('connect-wwwhisper');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/institute-of-directors'));
-// app holds a reference to express or connect framework, it
-// may be named differently in your source file.
-app.use(wwwhisper());
 
 app.get('/*', function(req,res) {
 
