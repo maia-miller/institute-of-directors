77d85dc3ab2794b594147a78ed769bedc2cdb97d
Maia Miller <maiamiller@catalyst.net.nz>
2019-05-08 21:19:42 +1200

    Reset nav to Apr 11, when last noted as functioning properly

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,11 +7,7 @@ import { Router, Event, NavigationStart } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  isMenuExpanded = false;
-
-  ShowWhoAreWe = false;
-  ShowOurYear = false;
-  ShowFaces = false;
+  isMenuExpanded: boolean = false;
 
   constructor(private router: Router) {
     router.events.subscribe( (event: Event) => {
@@ -33,46 +29,13 @@ export class NavbarComponent implements OnInit {
       return navbarClass = 'homepageNavBar'
     } else if (url === '/our-branch-network') {
       return navbarClass = 'bluePageNavBar'
-    } else if (screenSize > 770) {
+    } else if (screenSize > 768) {
       return navbarClass = 'desktopNavBar'
     }
   }
 
   toggleMenu() {
-    this.ShowFaces = false;
-    this.ShowOurYear = false;
-    this.ShowWhoAreWe = false;
-    this.isMenuExpanded = !this.isMenuExpanded;
-  }
-
-  isTabDark() {
-    return window.location.pathname === '/home' ? 'darkTab' : 'lightTab'
-  }
-
-
-  toggleWhoAreWe() {
-    this.ShowWhoAreWe = !this.ShowWhoAreWe;
-    this.ShowFaces = false;
-    this.ShowOurYear = false;
-  }
-
-  toggleOurYear() {
-    this.ShowOurYear = !this.ShowOurYear;
-    this.ShowFaces = false;
-    this.ShowWhoAreWe = false;
-  }
-
-  toggleShowFaces() {
-    this.ShowFaces = !this.ShowFaces;
-    this.ShowOurYear = false;
-    this.ShowWhoAreWe = false;
-  }
-
-  isDesktop() {
-    if (screen.width > 770) {
-      return true;
-    }
-    return false;
+    this.isMenuExpanded = !this.isMenuExpanded
   }
 
 }
--- src/app/components/navbar/navigation-links/navigation-links.component.ts
+++ src/app/components/navbar/navigation-links/navigation-links.component.ts
@@ -9,11 +9,6 @@ export class NavigationLinksComponent implements OnInit {
 
   constructor() { }
 
-  ShowWhoAreWe = false;
-  ShowOurYear = false;
-  ShowFaces = false;
-
-
   ngOnInit() {
   }
 
@@ -21,19 +16,4 @@ export class NavigationLinksComponent implements OnInit {
     return window.location.pathname === '/home' ? 'darkTab' : 'lightTab'
   }
 
-  toggleWhoAreWe() {
-    this.ShowWhoAreWe = !this.ShowWhoAreWe;
-  }
-
-  toggleOurYear() {
-    this.ShowOurYear = !this.ShowOurYear;
-  }
-
-  toggleShowFaces() {
-    this.ShowFaces = !this.ShowFaces;
-  }
-
-  isDesktop() {
-    return screen.width > 770;
-  }
 }
